6a2b6c109324365e2c8d86d691a0f19b61cd6433
Rob Banagale <rob@banagale.com>
2025-11-05 12:51:42 -0800

    build: add swift-async-algorithms dependency for multicast support

--- Package.swift
+++ Package.swift
@@ -10,13 +10,15 @@ let package = Package(
     .library(name: "ContextifyCore", targets: ["ContextifyCore"]),
   ],
   dependencies: [
-    .package(url: "https://github.com/groue/GRDB.swift.git", from: "6.29.0")
+    .package(url: "https://github.com/groue/GRDB.swift.git", from: "6.29.0"),
+    .package(url: "https://github.com/apple/swift-async-algorithms.git", from: "1.0.0")
   ],
   targets: [
     .target(
       name: "ContextifyCore",
       dependencies: [
-        .product(name: "GRDB", package: "GRDB.swift")
+        .product(name: "GRDB", package: "GRDB.swift"),
+        .product(name: "AsyncAlgorithms", package: "swift-async-algorithms")
       ],
       path: "app/Sources/ContextifyCore",
       swiftSettings: [
