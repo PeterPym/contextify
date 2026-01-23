f453235fe13575b9887e361abcfc52b9afebecc6
Rob Banagale <rob@banagale.com>
2026-01-22 16:47:08 -0800

    fix(cli): update dev version to 1.2.1-dev
    Match the current release version number for local development builds.

--- Sources/ContextifyCLI/main.swift
+++ Sources/ContextifyCLI/main.swift
@@ -21,7 +21,7 @@ import ContextifyIngestionCommands
 #if GENERATED_VERSION
 let cliVersion = generatedCLIVersion
 #else
-let cliVersion = "1.1.0-dev"
+let cliVersion = "1.2.1-dev"
 #endif
 
 // MARK: - Deprecation Warnings
